eff2cc3e68b3ddce9fb1643c91927b3c24cdbfaa
Dominik <dominik.kantorowicz@gmail.com>
2022-12-16 20:40:58 +0100

    flex-wrap to popups

--- components/Prices.js
+++ components/Prices.js
@@ -2,7 +2,7 @@ import prices from "../locales/prices";
 import ModalElement from "./Modal";
 
 const Prices = () => {
-    return <div className={"flex xs:flex-com sm:flex-row justify-center items-center gap-x-3"}>
+    return <div className={"flex xs:flex-com sm:flex-row justify-center items-center gap-x-3 flex-wrap"}>
         {prices.map(el => (
             <ModalElement prices={el} key={el.id}/>
         ))}
