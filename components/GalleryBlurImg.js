ca5cac7717793a74aeb46052ae391e8747be7a1b
Dominik <dominik.kantorowicz@gmail.com>
2022-12-21 09:21:14 +0100

    Image styling

--- components/GalleryBlurImg.js
+++ components/GalleryBlurImg.js
@@ -1,18 +1,21 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import {useState} from "react";
+import {useRouter} from "next/router";
 
 const classesImg = (...classes) => {
     return classes.filter(Boolean).join(' ')
 }
 
 const GalleryBlurImg = ({image}) => {
+    const routerTranslation = useRouter();
+    const {locale} = routerTranslation;
     const [isLoading, setLoading] = useState(true)
     return (
         <>
-            <Link href={"/"}>
+            <Link href={locale === 'pl' && "/cennik" || locale !== 'pl' && "/en/cennik"}>
                 <div className={"w-full overflow-hidden rounded-lg shadow-xl group"}>
-                    <Image src={image.imgHref} alt={"Wait for loading img"} width={637} height={850} className={classesImg(
+                    <Image src={image.imgHref} alt={locale === 'pl' && image.servicesPl || locale !== 'pl' && image.servicesEn} width={637} height={850} className={classesImg(
                         'duration-300 ease-in-out group-hover:opacity-75',
                         isLoading
                             ? 'scale-110 blur-2xl grayscale'
@@ -21,8 +24,7 @@ const GalleryBlurImg = ({image}) => {
                            onLoadingComplete={() => setLoading(false)}
                     />
                 </div>
-                <h3 className={"mt-2 text-sm text-gray-700 dark:text-white font-semibold"}>{image.servicesPl}</h3>
-                <p className={"mt-0 text-lg font-medium text-gray-800 dark:text-white"}>Ipsum dolorem</p>
+                <h2 className={"mt-2 text-sm text-gray-700 dark:text-white font-semibold text-center"}>{locale === 'pl' && image.servicesPl || locale !== 'pl' && image.servicesEn}</h2>
             </Link>
         </>
 
