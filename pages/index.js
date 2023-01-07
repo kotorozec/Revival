038dd2bdb0ab35f7de4f22a5246929b3afe01c7f
Dominik <dominik.kantorowicz@gmail.com>
2023-01-07 19:20:23 +0100

    mobile main page services fix

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const Home = () => {
                 <div className={"hidden absolute -bottom-4 md:bottom-0 left-10 xl:block"}>
                     <Image src={dotsSmall} width={50} height={50} alt={""} className={"rotate-dots-bottom"}></Image>
                 </div>
-                <div className={"grid grid-cols-1 md:grid-cols-2 w-full h-full py-10 place-items-center p-5 "}>
+                <div className={"grid grid-cols-1 md:grid-cols-2 w-full h-full lg:py-10 place-items-center"}>
                     <div className={"place-items-center grid grid-cols-1 relative"}>
                         <h1 className={"text-gray-600 font-semibold dark:text-white text-4xl pb-4 w-full"}>{t.MainTitle}</h1>
                         <TypingMainPage/>
@@ -38,44 +38,44 @@ const Home = () => {
                         </p>
 
                         <h2 className={"pb-6 dark:text-white w-full xl:text-center "}>{t.MainServicesIntro}</h2>
-                        <div className={"w-full grid grid-cols-1  place-items-center"}>
-                            <div className={"grid grid-cols-2 md:grid-cols-2 gap-5 pb-6 w-3/4"}>
+                        <div className={"w-full grid grid-cols-1 place-items-center"}>
+                            <div className={"grid grid-cols-2 sm:grid-cols-3 gap-5 pb-6 xl:w-3/4"}>
                                 <div
                                     className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-[#ffa500] rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-full"}>{t.MainServicesFirst}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesFirst}</h3>
                                 </div>
                                 <div
-                                    className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800 shadow-xl "}>
+                                    className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-gray-800 rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-full"}>{t.MainServicesSecond}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesSecond}</h3>
                                 </div>
                                 <div
-                                    className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800 shadow-xl "}>
+                                    className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-[#ffa500] rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-full"}>{t.MainServicesThird}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesThird}</h3>
                                 </div>
                                 <div
-                                    className={"bg-white rounded-full py-3 flex items-center justify-evenly gap-1 dark:text-gray-800 shadow-xl "}>
+                                    className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-gray-800 rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-3/4"}>{t.MainServicesForth}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesForth}</h3>
                                 </div>
 
                                 <div
                                     className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-[#ffa500] rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-full"}>{t.MainServicesFifth}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesFifth}</h3>
                                 </div>
                                 <div
                                     className={"bg-white rounded-full p-3 flex items-center justify-evenly gap-1 dark:text-gray-800  shadow-xl "}>
                                     <div
-                                        className="min-h-[0.8rem] min-w-[0.8rem] bg-gray-800 rounded-full before:content-[''] mr-2"></div>
-                                    <h3 className={"text-base w-full"}>{t.MainServicesSixth}</h3>
+                                        className="min-h-[0.7rem] min-w-[0.7rem] bg-gray-800 rounded-full before:content-[''] mx-1"></div>
+                                    <h3 className={"text-base w-full text-center"}>{t.MainServicesSixth}</h3>
                                 </div>
                             </div>
                         </div>
